5cc67326f620791cbaab7eca61a25a5ffbf09044
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-11-27 11:51:45 +0900

    feat: add demo page with download btns

---
+++ backend/templates/Pages/demo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSV/TSV Demo</title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'home']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->Html->script(['https://cdn.tailwindcss.com']) ?>
+    <?= $this->fetch('script') ?>
+</head>
+
+<body>
+    <div class="p-10">
+        <table>
+            <tr>
+                <th>
+                    <span>CSV Download</span>
+                </th>
+                <td>
+                    <button>Download</button>
+                </td>
+            </tr>
+
+            <tr>
+                <th>
+                    <span>TSV Download</span>
+                </th>
+                <td>
+                    <button>Download</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
